1e3900bdfdd6b216e73d8021c514b149135eb88c
Joako-G <gallardojoako@gmail.com>
2024-02-23 11:16:49 -0300

    Ultimas modificaciones en el diseño del juego

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -4,9 +4,9 @@ const Start = ({ startGame }) => {
   }
   return (
     <div className='start-game'>
-      <h1 className='title'>Bienvenidos</h1>
+      <h1 className='title'>Bienvenidos a Trivia Pokemon </h1>
       <img className='image' src='https://i.blogs.es/82d7ef/pokemon/1366_2000.jpeg' alt='' />
-      <p>El juego es simple, consiste en adivinar el nombre del pokemon de la imagen.</p>
+      <p>El juego consiste en adivinar el nombre del pokemon de la imagen. Cada 10 pokemons va cambiando la dificultad del juego</p>
       <button className='start' onClick={handleClick}>Comenzar Juego</button>
     </div>
   )
