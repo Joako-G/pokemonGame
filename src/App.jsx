337ce5bfeac14880577197a6ba23d2dc813bf0a7
Joako-G <gallardojoako@gmail.com>
2024-03-11 10:50:52 -0300

    Optimizacion y mejoras en el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Start from './Components/Start'
 import Game from './Components/Game'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
-
-// AGREGO RAMA DEVELOP PARA REALIZAR MEJORAS Y ACTUALIZACIONES DEL JUEGO
 
 function App () {
   // Para iniciar el juego
   const [inGame, setInGame] = useState(false)
 
   const startGame = () => {
-    setInGame(true) // Para iniciar el juego
+    setInGame(true)
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <div className='App'>
         {inGame
-          ? (
-
-            <Game />
-
-            )
+          ? (<Game />)
           : (<Start startGame={startGame} />)}
       </div>
-    </QueryClientProvider>
+    </>
   )
 }
 
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -4,31 +4,29 @@ import WinnerModal from './WinnerModal'
 import { usePokemons } from '../Hooks/usePokemons'
 import { mixPokemonsInGame } from '../Logic/MixPokemonInGame'
 import { usePokemonImage } from '../Hooks/usePokemonImage'
+import { useVerify } from '../Hooks/useVerify'
+
+// CRAEAR UN COMPONENTE LLAMADO MODAL EL CUAL CONTENDRA 2 COMPONENTES:
+// COMPONENTE CONTINUEMODAL Y ENDGAMEMODAL
 
 const Game = () => {
   const [pokemonsIngGame, setPokemonsInGame] = useState([]) // Lista de pokemons para el juego(4)
-  const [guessedPokemon, setGuessedPokemon] = useState(null) // Pokemon elegido por el jugador
 
-  const [guessedAmount, setGuessedAmount] = useState(0)
-  const [siguiente, setSiguiente] = useState(false)
-
-  const [gameOver, setGameOver] = useState(false)
   const [showModal, setShowModal] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
 
   /* NUEVO CODIGO */
-  const { pokemonsList, amount, nextPage, refreshPokemons, refreshAmount } = usePokemons()
-
-  const { imgPokemon, pokemon, refreshIndex, index } = usePokemonImage({ pokemons: pokemonsList })
+  const { pokemonsList, nextPage, refreshPokemons } = usePokemons()
+  const { pokemon, refreshIndex, index } = usePokemonImage({ pokemons: pokemonsList })
+  const { guessedPokemon, guessedAmount, gameOver, siguiente, checkName, continueGame, nextGame } = useVerify()
 
   useEffect(() => {
     setIsLoading(true)
-
-    if (pokemonsList && pokemonsList.length > 0 && pokemon) {
+    if (pokemonsList && pokemonsList.length > 0) {
       const newList = mixPokemonsInGame(pokemon, pokemonsList)
       setPokemonsInGame(newList)
     }
-  }, [pokemonsList, pokemon])
+  }, [pokemon])
 
   useEffect(() => {
     if (gameOver) {
@@ -45,61 +43,28 @@ const Game = () => {
     }
   }, [gameOver, isLoading])
 
-  /**
-   * Funcion que verifica si el usuario adivino el nombre del pokemon.
-   * Recibe por parametro el nombre del pokemon seleccionado por el
-   * jugador
-   * @param {*} name
-   */
-  const checkName = (name) => {
-    if (index === pokemonsList.length - 1) {
-      setGameOver(true)
-    }
-    setGuessedPokemon(pokemon.name)
-
-    if (name === pokemon.name) {
-      const newGuessedAmount = guessedAmount + 1
-      setGuessedAmount(newGuessedAmount)
-      console.log('CORRECT')
-    }
-
-    setSiguiente(true)
+  const verify = (name) => {
+    checkName(name, pokemon, pokemonsList, index)
   }
 
-  /**
-   * Metodo para pasar a la siguiente ronda
-   */
-  const continueGame = () => {
-    setGuessedPokemon(null)
-    setSiguiente(false)
-
-    refreshAmount()
-    refreshIndex()
-  }
-
-  const nextGame = () => {
-    setGameOver(false)
-    setGuessedPokemon(null)
-    setSiguiente(false)
+  const restartGame = () => {
+    nextGame(refreshIndex, refreshPokemons, nextPage)
     setShowModal(false)
-    refreshPokemons(nextPage)
-    refreshIndex()
-    setGuessedAmount(0)
   }
 
   return (
     <div className='game'>
-      <h4>Faltan: {amount} </h4>
+      <h4>Faltan: {pokemonsList.length - index} </h4>
       <div className='img-pokemon'>
         {isLoading && (<h1>Cargando imagen</h1>)}
-        {!isLoading && (<img src={imgPokemon} alt='Image de pokemon' />)}
+        {!isLoading && (<img src={pokemon.img} alt='Image de pokemon' />)}
       </div>
-      <PokemonList pokemonsInGame={pokemonsIngGame} checkName={checkName} guessedPokemon={guessedPokemon} />
+      <PokemonList pokemonsInGame={pokemonsIngGame} checkName={verify} guessedPokemon={guessedPokemon} />
       {
-        siguiente && !gameOver && (<button className='siguiente' onClick={continueGame}> Continuar </button>)
+        siguiente && !gameOver && (<button className='siguiente' onClick={() => continueGame(refreshIndex)}> Continuar </button>)
       }
       {
-        gameOver && showModal && (<WinnerModal guessedAmount={guessedAmount} restartGame={nextGame} gameOver={gameOver} />)
+        gameOver && showModal && (<WinnerModal guessedAmount={guessedAmount} restartGame={() => restartGame(refreshIndex, refreshPokemons, nextPage)} gameOver={gameOver} />)
       }
     </div>
   )
